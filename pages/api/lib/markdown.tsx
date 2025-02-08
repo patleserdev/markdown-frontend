3263bda1f0441340337945d5f5a899b9a448cb1c
patleserdev <leserpat@hotmail.com>
2025-02-08 14:24:18 +0100

    debug mode

--- pages/api/lib/markdown.tsx
+++ pages/api/lib/markdown.tsx
@@ -3,8 +3,13 @@ import path from "path"; // Module pour gérer les chemins de fichiers
 
 
 // Récupère les noms des fichiers Markdown dans un répertoire donné
-export function getMarkdownFiles(directory = "/markdown") {
-  const dirPath = path.join("./public", directory); // Utiliser ./public
+export function getMarkdownFiles(directory = "public/markdown") {
+  const dirPath = path.resolve(process.cwd(), directory); // Utiliser un chemin absolu
+  console.log("Checking path:", dirPath);
+  if (!fs.existsSync(dirPath)) {
+    console.error("Directory does not exist:", dirPath);
+    return [];
+  }
     try {
     const filenames = fs.readdirSync(dirPath); // Liste des fichiers
     return filenames
