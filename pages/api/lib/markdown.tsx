6832806e4bdae373e79701af5809e1cd8df2e324
patleserdev <leserpat@hotmail.com>
2025-02-08 14:14:00 +0100

    change get navbar files method

--- pages/api/lib/markdown.tsx
+++ pages/api/lib/markdown.tsx
@@ -1,13 +1,17 @@
 import fs from "fs"; // Module pour lire les fichiers
 import path from "path"; // Module pour gérer les chemins de fichiers
-import matter from "gray-matter"; // Librairie pour lire du Markdown avec des métadonnées
 
 
 // Récupère les noms des fichiers Markdown dans un répertoire donné
-export function getMarkdownFiles(directory = "./public/markdown") {
+export function getMarkdownFiles(directory = "public/markdown") {
   const dirPath = path.join(process.cwd(), directory); // Chemin absolu
-  const filenames = fs.readdirSync(dirPath); // Liste des fichiers
-  return filenames
-    .filter((file) => file.endsWith(".md")) // Ne garder que les fichiers .md
-    .map((file) => file.replace(".md", "")); // Supprimer l'extension .md
+  try {
+    const filenames = fs.readdirSync(dirPath); // Liste des fichiers
+    return filenames
+      .filter((file) => file.endsWith(".md")) // Ne garder que les fichiers .md
+      .map((file) => file.replace(".md", "")); // Supprimer l'extension .md
+  } catch (error) {
+    console.error("Error reading directory:", error);
+    return [];
+  }
 }
