c2bd72d1f0a4e9c5a1718754cb27060defaea603
patleserdev <leserpat@hotmail.com>
2025-02-08 14:16:20 +0100

    change method

--- pages/api/lib/markdown.tsx
+++ pages/api/lib/markdown.tsx
@@ -1,17 +1,21 @@
-import fs from "fs"; // Module pour lire les fichiers
-import path from "path"; // Module pour gérer les chemins de fichiers
+import fs from "fs";
+import path from "path";
 
+export async function getStaticProps() {
+  const directory = "public/markdown";
+  const dirPath = path.join(process.cwd(), directory);
+  let markdownFiles:string[] = [];
 
-// Récupère les noms des fichiers Markdown dans un répertoire donné
-export function getMarkdownFiles(directory = "public/markdown") {
-  const dirPath = path.join(process.cwd(), directory); // Chemin absolu
   try {
-    const filenames = fs.readdirSync(dirPath); // Liste des fichiers
-    return filenames
-      .filter((file) => file.endsWith(".md")) // Ne garder que les fichiers .md
-      .map((file) => file.replace(".md", "")); // Supprimer l'extension .md
+    const filenames = fs.readdirSync(dirPath);
+    markdownFiles = filenames.filter((file) => file.endsWith(".md"));
   } catch (error) {
-    console.error("Error reading directory:", error);
-    return [];
+    console.error("Error reading markdown files:", error);
   }
+
+  return {
+    props: {
+      files: markdownFiles,
+    },
+  };
 }
